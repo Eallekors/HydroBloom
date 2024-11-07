7184de2f4d0cf24309c9b8b99b7425abe689ba00
Eallekors <Erkxalle@gmail.com>
2024-11-07 14:45:01 +0200

    Created a container component

--- app/index.jsx
+++ app/index.jsx
@@ -2,14 +2,17 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button, Text, View } from "react-native";
 import { router, Stack } from "expo-router";
+import Container from "../components/Container.jsx";
 
 export default function Index() {
   return (
     <SafeAreaView>
-      <Text>Hello world!</Text>
-      <Button title="Login" onPress={() => router.push('/Auth/login')} />
-      <Button title="Sign up" onPress={() => router.push('/Auth/signup')} />
-    
+      <Container >
+        <Text>Hello world!</Text>
+        <Button title="Login" onPress={() => router.push('/Auth/login')} />
+        <Button title="Sign up" onPress={() => router.push('/Auth/signup')} />
+       
+      </Container>
     </SafeAreaView>
   );
 }
