f695e2f0d3a55624fa84e9fad85690d28da1d8a0
Eallekors <Erkxalle@gmail.com>
2024-11-07 15:19:24 +0200

    Fixed header merge issue

--- app/index.jsx
+++ app/index.jsx
@@ -3,11 +3,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Button, Text, View } from "react-native";
 import { router, Stack } from "expo-router";
 import Container from "../components/Container.jsx";
-
+import Header from "../components/Header.jsx"
 export default function Index() {
   return (
     <SafeAreaView>
       <Container >
+        <Header title="SPlash"  showBack={true}/>
         <Text>Hello world!</Text>
         <Button title="Login" onPress={() => router.push('/Auth/login')} />
         <Button title="Sign up" onPress={() => router.push('/Auth/signup')} />
