dfde3865804156798011f1739c789b3903848f96
karl <karlkivisild@gmail.com>
2024-11-07 15:09:23 +0200

    fixed header position

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Pressable, Image, Text, View, StyleSheet } from "react-native";
 
-// Move styles outside the component to ensure it's created only once
-export default function Header ({ title, onBackPress, onLogout, showBack, showLogout }) {
+export default function Header({ title, onBackPress, onLogout, showBack, showLogout }) {
   return (
     <View style={styles.container}>
       {showBack && (
-        <Pressable hitSlop={20} onPress={onBackPress}>
+        <Pressable hitSlop={20} onPress={onBackPress} style={styles.backButton}>
           <Image style={styles.icon} source={require('../assets/images/Arrow_left.png')} />
         </Pressable>
       )}
       <Text style={styles.title}>{title}</Text>
       {showLogout && (
-        <Pressable hitSlop={20} onPress={onLogout}>
+        <Pressable hitSlop={20} onPress={onLogout} style={styles.logoutButton}>
           <Image style={styles.icon} source={require('../assets/images/Sign_out_square.png')} />
         </Pressable>
       )}
@@ -20,14 +19,15 @@ export default function Header ({ title, onBackPress, onLogout, showBack, showLo
   );
 };
 
-// Styles are now outside the component to avoid re-creation on every render
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 20,
-    borderBottomWidth: 1.9, 
-    borderBottomColor: "#D3D3D3"
+    padding: 5,
+    borderBottomWidth: 1.9,
+    borderBottomColor: "#D3D3D3",
+    position: "relative", 
   },
   icon: {
     width: 34,
@@ -35,10 +35,17 @@ const styles = StyleSheet.create({
     tintColor: "#000",
   },
   title: {
-    flex: 1,   
+    flex: 1, 
     textAlign: "center", 
     fontSize: 20,
     fontWeight: "bold",
   },
+  backButton: {
+    position: "absolute", 
+    left: 20,
+  },
+  logoutButton: {
+    position: "absolute",
+    right: 20,
+  },
 });
-
