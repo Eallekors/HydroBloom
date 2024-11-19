db2ca5ff0d12011a4a8d8ec2304ba9142b9ff4b8
karl <karlkivisild@gmail.com>
2024-11-19 10:13:00 +0200

    Prevented able to go back to log in screen from Home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,23 @@
 import { Text, View, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import { BackHandler } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 
 const Home = () => {
+  useEffect(() => {
+    const backAction = () => {
+        // Prevent going back
+        return true;
+    };
 
+    const backHandler = BackHandler.addEventListener(
+        'hardwareBackPress',
+        backAction
+    );
+
+    return () => backHandler.remove();
+}, []);
 
   return (
     <SafeAreaView style={{ backgroundColor:'#fff'}}>
