cf3f1665fa4a041a1f652105f02403bd454dab89
Eallekors <Erkxalle@gmail.com>
2024-11-28 11:15:24 +0200

    Fixed merge errors

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -117,15 +117,9 @@ const Home = () => {
               <Text style={styles.text}>
                 xxx ml to go
               </Text>
-              <Pressable
-                style={({ pressed }) => [
-                  styles.drinkButton,
-                  { opacity: pressed ? 0.9 : 1 }
-                ]}
-                onPress={handlePress}
-              >
+              
                 {/* Circle Button */}
-      <TouchableOpacity style={styles.circleButton} onPress={toggleModal}>
+      <TouchableOpacity style={styles.drinkButton} onPress={toggleModal}>
         <Image style={styles.icon} source={require('../../assets/icons/water-glass.png')} />
       </TouchableOpacity>
 
@@ -151,7 +145,7 @@ const Home = () => {
         onClose={toggleDeleteModal}
         onDelete={(updatedData) => setButtons(updatedData)}
       />
-              </Pressable>
+              
             </View>
           )
         )}
@@ -161,7 +155,6 @@ const Home = () => {
 };
 
 const styles = StyleSheet.create({
-  
   view: {
     flex: 1,
   },
@@ -187,56 +180,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  container: {
-    position: 'absolute',
-    top: '0%', // Positioned 10% from the top of the screen
-    left: '50%', // Align horizontally to the center
-    marginLeft: -(width * 0.6) / 2, // Offset the container by half its width to center it
-    width: width * 0.6, // Make container 80% of the screen width
-    height: height * 0.15, // Set the container height to 20% of the screen height
-    justifyContent: 'center', // Center the content vertically within the container
-  },
-  containerText: {
-    fontSize: 16,
-    color: '#000',
-    textAlign: 'left',
-  },
-  containerStats: {
-    backgroundColor: '#3498db', // Background color for the stats
-    color: '#fff', // Text color inside the stats
-    width: 10,
-    height: 10,
-    paddingVertical: 5, // Padding to give the background some height
-    paddingHorizontal: 15, // Padding to make it rounded
-    borderRadius: 5, // Rounded corners
-    fontWeight: 'bold', // Bold the number
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 20,
-    color: '#fff',
-  },
-  circleButton: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  icon: {
-    width: 32,
-    height: 32,
   },
   content: {
     flex: 1,
@@ -317,5 +260,6 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   }
 });
+
 
 export default Home;
